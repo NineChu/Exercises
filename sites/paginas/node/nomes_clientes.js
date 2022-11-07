a6f048345ef69b5e7288108ae5a394946a836817
ninechu0@gmail.com <notebook>
2022-11-07 11:35:55 -0300

    new

--- sites/paginas/node/nomes_clientes.js
+++ sites/paginas/node/nomes_clientes.js
@@ -1,2 +1,3 @@
 const clientes = require("./clientes");
-clientes.findAll({ attributes: ["nome"] }).then((clientes) => clientes.forEach((cliente) => console.log(cliente.dataValues.nome)));
+var x = ''
+clientes.findAll({ attributes: ["nome"] }).then((clientes) => clientes.forEach((cliente) => x += cliente.dataValues.nome));
